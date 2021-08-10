d0efde844212900eab7f43d442d5c34af2121dab
David Federspiel <david@federnet.com>
2021-08-10 01:43:07 -0400

    feat: add jest

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+/** @type {import('@ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  collectCoverageFrom: [
+    'api/src/**/*.{js,jsx,ts,tsx}',
+    'graphql/src/**/*.{js,jsx,ts,tsx}',
+    'www/src/**/*.{js,jsx,ts,tsx}',
+    '!**/node_modules/**',
+  ],
+};
---
+++ jest.setup.js
@@ -0,0 +1,15 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const crypto = require('crypto');
+
+const mockFetchPromise = Promise.resolve({
+  ok: true,
+  json() {
+    return { ok: 'yay' };
+  },
+});
+
+global.fetch = jest.fn().mockImplementation(() => mockFetchPromise);
+
+window.crypto = {
+  getRandomValues: (buffer) => crypto.randomFillSync(buffer),
+};
