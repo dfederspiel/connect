e7ec421996a77c8e625d45f4659e8059beecf790
David Federspiel <david@federnet.com>
2021-08-10 02:16:11 -0400

    fix: update volume mapping and invalid refs

--- api/src/controllers/UserController.ts
+++ api/src/controllers/UserController.ts
@@ -1,15 +1,12 @@
 import { User } from '@prisma/client';
 import { Router } from 'express';
-import { IUserDataContext } from '../data/UserDataContext';
 
 export default class UserController {
   private path = '/users';
   private router: Router;
-  private context: IUserDataContext;
 
-  constructor(router: Router, context: IUserDataContext) {
+  constructor(router: Router) {
     this.router = router;
-    this.context = context;
     this.initializeRoutes();
   }
 
@@ -18,6 +15,6 @@ export default class UserController {
   };
 
   private getAllUsers = async (req: any, res: { json: (users: User[]) => void }) => {
-    res.json(await this.context.getAll());
+    res.json([]);
   };
 }
