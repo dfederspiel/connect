951bfac96ee8250f9de350b5324bab0feb082785
David Federspiel <david@federnet.com>
2021-08-10 14:16:26 -0400

    clean: disable logging untill common logger is in place

--- www/src/context/AuthenticationContext/constants.ts
+++ www/src/context/AuthenticationContext/constants.ts
@@ -14,29 +14,29 @@ export const MSAL_CONFIG: Configuration = {
     cacheLocation: 'sessionStorage',
     storeAuthStateInCookie: false,
   },
-  system: {
-    loggerOptions: {
-      loggerCallback: (level, message, containsPii) => {
-        if (containsPii) {
-          return;
-        }
-        switch (level) {
-          case LogLevel.Error:
-            console.error(message);
-            return;
-          case LogLevel.Info:
-            console.info(message);
-            return;
-          case LogLevel.Verbose:
-            console.debug(message);
-            return;
-          case LogLevel.Warning:
-            console.warn(message);
-            return;
-        }
-      },
-    },
-  },
+  // system: {
+  //   loggerOptions: {
+  //     loggerCallback: (level, message, containsPii) => {
+  //       if (containsPii) {
+  //         return;
+  //       }
+  //       switch (level) {
+  //         case LogLevel.Error:
+  //           console.error(message);
+  //           return;
+  //         case LogLevel.Info:
+  //           console.info(message);
+  //           return;
+  //         case LogLevel.Verbose:
+  //           console.debug(message);
+  //           return;
+  //         case LogLevel.Warning:
+  //           console.warn(message);
+  //           return;
+  //       }
+  //     },
+  //   },
+  // },
 };
 
 export const LOGIN_REQUEST = {
