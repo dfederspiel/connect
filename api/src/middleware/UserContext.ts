9c77080ec0f9fd254dad8d14047f63720ee98020
David Federspiel <david@federnet.com>
2021-08-12 00:57:16 -0400

    tests: user context

--- api/src/middleware/UserContext.ts
+++ api/src/middleware/UserContext.ts
@@ -1,12 +1,8 @@
-import express, { NextFunction } from 'express';
+import { Request, Response, NextFunction } from 'express';
 
-export const UserContext = (
-  req: express.Request,
-  res: express.Response,
-  next: NextFunction,
-) => {
-  const user = res.locals.user;
-  console.log('[USER OBJECT]', user);
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+export const UserContext = (_req: Request, res: Response, next: NextFunction) => {
+  const user = res.locals?.user;
   if (user) next();
   else res.sendStatus(403);
 };
---
+++ api/src/middleware/__tests__/UserContext.test.ts
@@ -0,0 +1,29 @@
+import { Request, Response } from 'express';
+import { UserContext } from '../UserContext';
+
+describe('the user context', () => {
+  it('allows traffic when user context is set', (done) => {
+    UserContext(
+      {} as unknown as Request,
+      {
+        locals: { user: 'david@codefly.ninja' },
+      } as unknown as Response,
+      () => {
+        done();
+      },
+    );
+  });
+  it('rejects traffic when user context is NOT set', (done) => {
+    UserContext(
+      {} as unknown as Request,
+      {
+        sendStatus: (status: number) => {
+          expect(status).toEqual(403);
+          done();
+        },
+      } as unknown as Response,
+      // eslint-disable-next-line @typescript-eslint/no-empty-function
+      () => { },
+    );
+  });
+});
