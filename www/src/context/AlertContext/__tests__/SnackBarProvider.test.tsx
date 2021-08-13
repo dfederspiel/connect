55e2a0347b1399e9670c1804220441d51755c797
David Federspiel <david@federnet.com>
2021-08-13 09:55:15 -0400

    clean: improve snackbar implementation

--- www/src/context/AlertContext/__tests__/SnackBarProvider.test.tsx
+++ www/src/context/AlertContext/__tests__/SnackBarProvider.test.tsx
@@ -1,14 +1,20 @@
+import { waitFor } from '@testing-library/react';
 import { renderHook, act } from '@testing-library/react-hooks';
 import { SnackBarProvider, useSnacks } from '../SnackBarProvider';
 
 describe('the toast provider', () => {
-  const wrapper = ({ children }: any) => <SnackBarProvider>{children}</SnackBarProvider>;
-  it('exists', () => {
+  const wrapper = ({ children }: any) => (
+    <SnackBarProvider timeout={1}>{children}</SnackBarProvider>
+  );
+  it('exists', async () => {
     const { result } = renderHook(() => useSnacks(), { wrapper });
     act(() => {
       result.current.updateMessage('test');
     });
     expect(result.current.message).toEqual('test');
     expect(result).toMatchSnapshot();
+    await waitFor(() => {
+      expect(result.current.open).toBeFalsy();
+    });
   });
 });
