9941e3398090631858f05b80abfa35bf11c57ea3
David Federspiel <david@codefly.ninja>
2023-07-15 18:13:46 -0400

    feat: remove api project

--- api/index.ts
+++
@@ -1,8 +0,0 @@
-import ApiServer from './src/server';
-import { test } from '@lib/tools';
-console.log(test('API'));
-
-const server = new ApiServer();
-server.start((message) => {
-  console.log(message);
-});
--- api/src/middleware/UserContext.ts
+++
@@ -1,8 +0,0 @@
-import { Request, Response, NextFunction } from 'express';
-
-// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-export const UserContext = (_req: Request, res: Response, next: NextFunction) => {
-  const user = res.locals?.user;
-  if (user) next();
-  else res.sendStatus(403);
-};
